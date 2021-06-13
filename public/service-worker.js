029b5e219843e91ad57593f6fcfbee002fd2d3bf
sg0703 <samgates@Sams-MBP.attlocal.net>
2021-06-12 20:41:50 -0400

    Removed unnecessary api cache, indexedDB is handling this

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,20 @@
+// configure static file cache
 const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
 
+// configure static files to cache
 const FILES_TO_CACHE = [
   '/',
   'index.html',
   'index.js',
   'styles.css',
   'service-worker.js',
-  'db.js',
+  '/assets/db.js',
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
 ];
 
-// install
+// install service worker
 self.addEventListener("install", function (evt) {
-  // pre cache all static assets
+  // cache static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
@@ -23,13 +24,13 @@ self.addEventListener("install", function (evt) {
   self.skipWaiting();
 });
 
-// activate
+// activate cache
 self.addEventListener("activate", function(evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          if (key !== CACHE_NAME) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
           }
@@ -41,30 +42,8 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 
-// fetch
+// send cached static files if offline
 self.addEventListener("fetch", function(evt) {
-  if (evt.request.url.includes("/api/transaction")) {
-    evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
-
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
-          });
-      }).catch(err => console.log(err))
-    );
-
-    return;
-  }
-
   evt.respondWith(
     caches.open(CACHE_NAME).then(cache => {
       return cache.match(evt.request).then(response => {
