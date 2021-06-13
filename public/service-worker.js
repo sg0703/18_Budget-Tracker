cfd7f0767814d7c0417f06bb1ab1d7794eb69873
sg0703 <samgates@Sams-MBP.attlocal.net>
2021-06-13 09:36:56 -0400

    Linked manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,8 +7,11 @@ const FILES_TO_CACHE = [
   'index.html',
   'index.js',
   'service-worker.js',
+  'manifest.webmanifest',
   '/assets/styles.css',
   '/assets/db.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ];
