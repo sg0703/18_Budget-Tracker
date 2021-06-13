1db887d6cf0e60c36f052fb555be2bf179b0556b
sg0703 <samgates@Sams-MBP.attlocal.net>
2021-06-12 21:33:46 -0400

    Moved styles to assets

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,8 +6,8 @@ const FILES_TO_CACHE = [
   '/',
   'index.html',
   'index.js',
-  'styles.css',
   'service-worker.js',
+  '/assets/styles.css',
   '/assets/db.js',
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
